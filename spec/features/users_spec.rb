810c1be317092a3f51864bf2723a54d75fbd8248
marthaberner <marthaberner.mb@gmail.com>
2014-05-28 11:24:51 -0600

    User sees an error when password is invalid

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -7,7 +7,7 @@ feature 'User Registratoin' do
     fill_in 'user[email]', with: 'joe@joe.com'
     fill_in 'user[password]', with: 'password1'
     fill_in 'user[password_confirmation]', with: 'password1'
-    click_on 'Create An Account'
+    click_on 'Create Account'
 
     expect(page).to have_content 'Welcome, joe@joe.com'
   end
@@ -18,9 +18,34 @@ feature 'User Registratoin' do
     fill_in 'user[email]', with: 'invaliduser@example,com'
     fill_in 'user[password]', with: 'password1'
     fill_in 'user[password_confirmation]', with: 'password1'
-    click_on 'Create An Account'
+    click_on 'Create Account'
 
     expect(current_path).to eq '/users'
     expect(page).to have_content 'Email is invalid'
+  end
+
+  scenario 'User sees an error if password is invalid' do
+    visit '/'
+
+    fill_in 'user[email]', with: 'invaliduser@example.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password1'
+    click_on 'Create Account'
+
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Password must be between 6 and 9 characters and have one number.'
+
+    fill_in 'user[password]', with: ''
+    fill_in 'user[password_confirmation]', with: ''
+    click_on 'Create Account'
+
+    expect(page).to have_content 'Password must be between 6 and 9 characters and have one number.'
+    expect(page).to have_content 'Password can\'t be blank'
+
+    fill_in 'user[password]', with: 'password1'
+    fill_in 'user[password_confirmation]', with: 'password'
+    click_on 'Create Account'
+
+    expect(page).to have_content 'Password confirmation doesn\'t match Password'
   end
 end
