d143cd353dc366934c0d902a0732c9cfa86073ad
marthaberner <marthaberner.mb@gmail.com>
2014-05-27 19:01:35 -0600

    User Authorization
    email must be valid

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.1.0'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.3'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 2.0'
+gem 'sdoc', '~> 0.4.0',          group: :doc
+gem 'devise', '~> 3.2.4'
+
+
+group :development, :test do
+  gem 'rspec-rails', '~> 2.14.2'
+  gem 'capybara', '~> 2.2.1'
+  gem 'database_cleaner', '~> 1.2.0'
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,71 @@
+require 'spec_helper'
+
+describe User do
+  it 'Email must be valid' do
+    user = User.new(
+      email: 'user1@example,com',
+      password: 'password1'
+    )
+    expect(user.valid?).to eq false
+  end
+
+  it 'Email cannot be blank' do
+    pending
+    user = User.new(
+      email: '',
+      password: 'password1'
+    )
+    expect(user.valid?).to eq false
+  end
+
+  it 'Password must be valid' do
+    pending
+    user = User.new(
+      email: 'user1@example.com',
+      password: 'password'
+    )
+    expect(user.valid?).to eq false
+  end
+
+  it 'Accepts password that starts with a number' do
+    pending
+    user = User.new(
+      email: 'user1@example.com',
+      password: '1password',
+      password_confirmation: '1password'
+    )
+    expect(user.valid?).to eq true
+  end
+
+  it 'Password must be at least 6 characters' do
+    pending
+    user = User.new(
+      email: 'user1@example.com',
+      password: '1pass',
+      password_confirmation: '1pass'
+    )
+    expect(user.valid?).to eq false
+  end
+
+  it 'Creates user with password confirmation' do
+    pending
+    user = User.new(
+      email: 'user1@example.com',
+      password: '1password',
+      password_confirmation: '1password',
+    )
+    expect(user.valid?).to eq true
+  end
+
+  it 'Requires a password confirmation' do
+    pending
+    user = User.new(
+      email: 'user1@example.com',
+      password: '1password',
+      password_confirmation: '',
+    )
+    expect(user.valid?).to eq false
+  end
+end
+
+
