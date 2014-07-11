b73497dfca17d0501469ad0524838bb3f913382a
marthaberner <marthaberner.mb@gmail.com>
2014-07-11 17:16:58 -0600

    Displayed title is a max of 36 characters long followed by ...

--- spec/features/bookmarks_spec.rb
+++ spec/features/bookmarks_spec.rb
@@ -4,8 +4,10 @@ feature 'User Bookmarks' do
   scenario 'Only a user\'s bookmarks display in their account' do
     user = create_user(email: 'sue@sue.com')
     user2 = create_user(email: 'joe@joe.com')
-    bookmark = create_bookmark(user.id, title: 'Some Awesome Thing', url: 'http://www.someverylongstupidurlwithlotsof12345.com')
     create_bookmark(user2.id, title: 'Some Other Awesome Thing')
+    bookmark = create_bookmark(user.id,
+                               title: 'Some Ver Long Dragging On Name That Goes On Forever Awesome Thing',
+                               url: 'http://www.someverylongstupidurlwithlotsof12345.com')
 
     visit '/'
 
@@ -15,7 +17,9 @@ feature 'User Bookmarks' do
       click_on 'Login'
     end
 
+    # displayed title is shortened
     expect(page).to have_content 'Hello, sue@sue.com'
+    expect(page).to have_content 'Some Ver Long Dragging On Name That ...'
     expect(page).to have_no_content 'Some Other Awesome Thing'
 
     # displayed url is shortened
